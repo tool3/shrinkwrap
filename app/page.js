7209c14d5b87a85df6fa68ac3a5858090a18f477
Tal Hayut <tal.hayut@bigpanda.io>
2024-12-12 22:21:01 +0200

    chore: dud

--- app/page.js
+++ app/page.js
@@ -9,8 +9,8 @@ import React, { Suspense, useEffect, useState } from 'react';
 import Credits from './components/Credit';
 import Debug from './components/Debug';
 import './index.scss';
-import Shrinkwrap from './components/Shrinkwrap';
-import Wrap from './components/Wrap';
+// import Shrinkwrap from './components/Shrinkwrap';
+// import Wrap from './components/Wrap';
 
 function Loader() {
     const { progress } = useProgress();
@@ -81,7 +81,7 @@ export default function Page() {
                 <Suspense fallback={<Loader />}>
                     {perf ? <Perf align="top-right" /> : null}
 
-                    <Wrap scale={2} />
+                    {/* <Wrap scale={2} /> */}
 
                     <Environment files="./textures/environments/studio_small_03_2k.hdr" environmentIntensity={0.3} resolution={720} />
                     <OrbitControls dampingFactor={0.1} autoRotate={rotate} minZoom={10} maxZoom={100} />
