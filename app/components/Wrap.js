b18e7e7c5d49c28aa328303985b87b094a3a9627
Tal Hayut <tal.hayut@bigpanda.io>
2024-12-13 08:02:02 +0200

    chore: no material

--- app/components/Wrap.js
+++ app/components/Wrap.js
@@ -4,30 +4,26 @@ import { MeshStandardMaterial } from 'three'
 
 export default function Wrap(props) {
     const { nodes, materials } = useGLTF('/models/wrap_1.glb')
-    
+
     return (
         <group {...props} dispose={null}>
             <mesh
                 castShadow
                 receiveShadow
                 geometry={nodes.Text.geometry}
-                material={new MeshStandardMaterial({  metalness: 0.3, roughness: 0.2, color: 'hotpink' })}
+                material={new MeshStandardMaterial({ metalness: 0.3, roughness: 0.2, color: 'hotpink' })}
                 rotation={[Math.PI / 2, 0, 0]}
             />
             <mesh
                 castShadow
                 receiveShadow
                 geometry={nodes.Cube.geometry}
-                material={nodes.Cube.material}
                 position={[0, 0.169, 0]}
                 scale={[1.964, 1, 1]}
             >
                 <meshStandardMaterial
-                    color="#ffffff"
-                    transmission={0.5}
-                    roughness={0.15}
-                    thickness={0.01}
-                    ior={1.5}
+                    color="red"
+                    transmission={0.2}
                 />
                 {/* <meshBasicMaterial color="red" /> */}
             </mesh>
