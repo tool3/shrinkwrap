c2281f10a640f6fb844b7f3f916cbbd644e5ba39
Tal Hayut <tal.hayut@bigpanda.io>
2024-12-13 08:05:14 +0200

    chore: refactor?

--- app/components/Wrap.js
+++ app/components/Wrap.js
@@ -5,28 +5,26 @@ import { MeshStandardMaterial } from 'three'
 export default function Wrap(props) {
     const { nodes, materials } = useGLTF('/models/wrap_1.glb')
 
+    const standard = useMemo(() => new MeshStandardMaterial({ metalness: 0.3, roughness: 0.2, color: 'hotpink' }), [])
+    const transmission = useMemo(() => <MeshTransmissionMaterial color="#ffffff" transmission={1} roughness={0.15} thickness={0.01} ior={1.5} />, [])
+
     return (
         <group {...props} dispose={null}>
             <mesh
                 castShadow
                 receiveShadow
                 geometry={nodes.Text.geometry}
-                material={new MeshStandardMaterial({ metalness: 0.3, roughness: 0.2, color: 'hotpink' })}
+                material={standard}
                 rotation={[Math.PI / 2, 0, 0]}
             />
             <mesh
                 castShadow
                 receiveShadow
                 geometry={nodes.Cube.geometry}
+                material={transmission}
                 position={[0, 0.169, 0]}
                 scale={[1.964, 1, 1]}
-            >
-                <meshStandardMaterial
-                    color="red"
-                    transmission={0.2}
-                />
-                {/* <meshBasicMaterial color="red" /> */}
-            </mesh>
+            />
         </group>
     )
 }
